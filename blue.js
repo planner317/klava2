5682e91da037a38531b5259c97f33d0f4b952643
planner317 <planner317@gmail.com>
2020-03-20 13:24:33 +0300

    добавил blue

---
+++ blue.js
@@ -0,0 +1,22 @@
+let playlist = {}
+playlist.text = `Yo, listen up, here's a story about a little guy that lives in a blue world. And all day and all night and everything he sees is just blue like him inside and outside. Blue his house with a blue little window and a blue Corvette and everything is blue for him and himself and everybody around cause he ain't got nobody to listen. I have a blue house with a blue window. Blue is the color of all that I wear. Blue are the streets and all the trees are too. I have a girlfriend and she is so blue. Blue are the people here that walk around. Blue like my Corvette that's standing outside. Blue are the words I say and what I think. Blue are the feelings that live inside me.`
+playlist.lang="eng"
+playlist.urlSound = "./music/Eiffel 65 - Blue.mp3";
+playlist.stop=[
+    {music:7.3, text: 77 },
+    {music:12.2, text: 140 },
+    {music:15.5, text: 168 },
+    {music:18.86, text: 209 },
+    {music:23.12, text: 260 },
+    {music:29.6, text: 330 },
+    {music:33, text: 370 },
+    {music:37.2, text: 408 },
+    {music:41.1, text: 456 },
+    {music:44.5, text: 496 },
+    {music:48, text: 539 },
+    {music:52.3, text: 586 },
+    {music:56.1, text: 630 },
+];
+playlist.volume = 0.7
+playlist.start = 0
+playlist.end = 59     // длина секунд до последей буквы текста
